e6933d35ef79defb779c8d4d5860ccfdcbfb6b0f
VanTruongUIT <tvtruong.uit@gmail.com>
2019-11-26 18:38:26 +0700

    Chuyen sang cach khac

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send('Response to source, hello you :)))');
+});
+
+router.get('/cool', (req, res, next) => {
+  res.send('Truong is so cool');
 });
 
 module.exports = router;
