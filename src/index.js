29b1fc4812b37af5bb6d89a5888adfcc97b155aa
VanTruongUIT <tvtruong.uit@gmail.com>
2019-11-30 10:59:57 +0700

    Co ban ve dinh tuyen da xong

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 let express = require('express');
+let path = require('path');
 
 let app = express();
 
 let personRoute = require('./routes/person');
 let userRoute = require('./routes/user');
 let publicRouter = require('./routes/public');
+
+app.use(express.static('../public'));
+
+app.get('/index.htm', function (req, res) {
+    res.sendFile('index.htm', {
+        root: path.join(__dirname, '../public')
+    });
+});
+app.get('/process_get', function (req, res) {
+    // Chuan bi output trong dinh dang JSON
+    response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
 
 app.use(personRoute);
 app.use(userRoute);
